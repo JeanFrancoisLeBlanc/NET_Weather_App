b7d05fb6ce0df966f50603862e1557e5ee8cc7ea
Jean-Francois LeBlanc <jeanleblancjfl@gmail.com>
2025-06-05 00:14:33 -0400

    change apikey

--- react_weather_app/Controllers/WeatherForecastController.cs
+++ react_weather_app/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get([FromQuery] string city = "New York")
     {
-        var apiKey = _config["WeatherApi:ApiKey"];
+        var apiKey = _config["API_KEY"];
         string url = $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}&aqi=no";
 
         var response = await _httpClient.GetAsync(url);
